1ccbf9dfafbb971258365e26bf071d83b5d15242
DanielDecembre <96702604+DanielDecembre@users.noreply.github.com>
2024-03-21 17:06:02 -0400

    grid-update

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,8 +1,24 @@
-export default function Page(){
+import React from 'react';
+import { Grid, GridItem } from '@chakra-ui/react'
+
+const Page: React.FC = () => {
     return (
         <div>
             This is Services
+            <Grid
+  h='200px'
+  templateRows='repeat(2, 1fr)'
+  templateColumns='repeat(5, 1fr)'
+  gap={4}
+>
+  <GridItem rowSpan={2} colSpan={1} bg='white' />
+  <GridItem colSpan={2} bg='grey' />
+  <GridItem colSpan={2} bg='grey' />
+  <GridItem colSpan={4} bg='white' />
+</Grid>
+            
         </div>
+    );
+};
 
-    )
-}
+export default Page; 
